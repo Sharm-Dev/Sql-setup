7e01b529e5c25ae2e11463dbace9aaf0c9f0b919
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-15 19:26:53 +0000

    add all files, created login in to server, and made get requets to localhost

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+app.use(express.json()); //This means the body will be automatically parsed from a string into a JSON object.
+
+const pool = new Pool({
+  user: "sharmainetaylor",
+  host: "localhost",
+  database: "sql_test",
+  password: "",
+  port: 5432,
+});
+
+// USE COMMAND LINE NOT WSL !!!!!
+app.listen(3000, function () {
+  console.log("Server is listening on port 6000. Ready to accept requests!");
+});
+
+app.get("/", function (req, res) {
+  console.log("Run...");
+
+  pool.query("SELECT * FROM customers ORDER BY name", (error, result) => {
+    res.json(result.rows); // THIS ONLY WORKS IN COMMAND LINE NOT WSL !!!!
+  });
+});
